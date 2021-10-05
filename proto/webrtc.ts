43de2d5c143784a52a92afbcb1051b7d714feac6
David DiMaria <info@collectivesessions.com>
2021-10-05 13:17:36 -0600

    CON-87: merge

--- proto/webrtc.ts
+++ proto/webrtc.ts
@@ -1,5 +1,5 @@
 import type * as grpc from '@grpc/grpc-js';
-import type { MessageTypeDefinition } from '@grpc/proto-loader';
+import type { EnumTypeDefinition, MessageTypeDefinition } from '@grpc/proto-loader';
 
 import type { WebRtcClient as _webrtc_WebRtcClient, WebRtcDefinition as _webrtc_WebRtcDefinition } from './webrtc/WebRtc';
 
@@ -16,6 +16,8 @@ export interface ProtoGrpcType {
   webrtc: {
     CreatePeerConnectionRequest: MessageTypeDefinition
     CreatePeerConnectionResponse: MessageTypeDefinition
+    CreateSDPRequest: MessageTypeDefinition
+    CreateSDPResponse: MessageTypeDefinition
     CreateSessionRequest: MessageTypeDefinition
     CreateSessionResponse: MessageTypeDefinition
     Empty: MessageTypeDefinition
@@ -23,7 +25,10 @@ export interface ProtoGrpcType {
     GetStatsResponse: MessageTypeDefinition
     PeerConnectionStat: MessageTypeDefinition
     PeerConnectionStats: MessageTypeDefinition
+    SDPType: EnumTypeDefinition
     SessionStats: MessageTypeDefinition
+    SetSDPRequest: MessageTypeDefinition
+    SetSDPResponse: MessageTypeDefinition
     StartSessionRequest: MessageTypeDefinition
     StopSessionRequest: MessageTypeDefinition
     WebRtc: SubtypeConstructor<typeof grpc.Client, _webrtc_WebRtcClient> & { service: _webrtc_WebRtcDefinition }
