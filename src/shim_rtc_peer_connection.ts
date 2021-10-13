804bab9decfc86027d9f4e6f3c2e346f0974d876
David DiMaria <info@collectivesessions.com>
2021-10-12 18:32:15 -0600

    CON-87: implement add_transceiver in the gRPC server + refactors

--- src/shim_rtc_peer_connection.ts
+++ src/shim_rtc_peer_connection.ts
@@ -255,7 +255,7 @@ export function setup(
   global.RTCPeerConnection = ShimRTCPeerConnection as any;
   global.navigator = {
     userAgent:
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36',
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3396.62 Safari/537.36',
   } as any;
   global.MediaStream = ShimMediaStream as any;
   (global as any).addTrackCallback = addTrackCallback;
