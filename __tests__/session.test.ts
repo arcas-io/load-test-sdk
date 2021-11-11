53e4702d9c7ca11115397ae6a01acaed959740fc
David DiMaria <info@collectivesessions.com>
2021-11-10 17:40:46 -0700

    CON-123: revert session tests in SDK

--- __tests__/session.test.ts
+++ __tests__/session.test.ts
@@ -9,30 +9,24 @@ async function newSession(): Promise<Session> {
   return await Session.create({ name: SESSION_NAME, servers: SERVERS });
 }
 
-describe('Session.create function', () => {
+describe('Session', () => {
   it('creates a new session', async () => {
     const session = await newSession();
     expect(session.id).toHaveLength(21);
     expect(session.name).toBe(SESSION_NAME);
   });
-});
 
-describe('Session.start function', () => {
   it('starts a session', async () => {
     const session = await newSession();
     await expect(session.start()).resolves.not.toThrow();
   });
-});
 
-describe('Session.stop function', () => {
   it('stops a session', async () => {
     const session = await newSession();
     await session.start();
     await expect(session.stop()).resolves.not.toThrow();
   });
-});
 
-describe('Session.createPeerConnection function', () => {
   it('creates a peer connection', async () => {
     const session = await newSession();
     await session.start();
@@ -40,9 +34,7 @@ describe('Session.createPeerConnection function', () => {
       session.createPeerConnection({ name: PEER_CONNECTION_NAME }),
     ).resolves.not.toThrow();
   });
-});
 
-describe('Session.createOffer function', () => {
   it('creates an offer', async () => {
     const session = await newSession();
     await session.start();
@@ -58,9 +50,7 @@ describe('Session.createOffer function', () => {
     // ensure that offer isn't blank
     expect(offer.sdp).toContain('v=0');
   });
-});
 
-describe('Session.createAnswer function', () => {
   it('creates an answer', async () => {
     const session = await newSession();
     await session.start();
@@ -87,9 +77,7 @@ describe('Session.createAnswer function', () => {
     // ensure that answer isn't blank
     expect(answerSdp.sdp).toContain('v=0');
   });
-});
 
-describe('Session.setLocalDescription function', () => {
   it('sets the local description', async () => {
     const session = await newSession();
     await session.start();
@@ -100,9 +88,7 @@ describe('Session.setLocalDescription function', () => {
 
     await expect(session.setLocalDescription(offer)).resolves.not.toThrow();
   });
-});
 
-describe('Session.setRemoteDescription function', () => {
   it('sets the remote description', async () => {
     const session = await newSession();
     await session.start();
@@ -118,37 +104,33 @@ describe('Session.setRemoteDescription function', () => {
     await expect(session.setRemoteDescription(options)).resolves.not.toThrow();
   });
 
-  describe('Session.addTrack function', () => {
-    it('adds a track', async () => {
-      const session = await newSession();
-      await session.start();
-      const { peer_connection_id }: any = await session.createPeerConnection({
-        name: PEER_CONNECTION_NAME,
-      });
-      await expect(
-        session.addTrack({
-          peer_connection_id,
-          track_id: '1',
-          track_label: 'FirstLabel',
-        }),
-      ).resolves.not.toThrow();
+  it('adds a track', async () => {
+    const session = await newSession();
+    await session.start();
+    const { peer_connection_id }: any = await session.createPeerConnection({
+      name: PEER_CONNECTION_NAME,
     });
+    await expect(
+      session.addTrack({
+        peer_connection_id,
+        track_id: '1',
+        track_label: 'FirstLabel',
+      }),
+    ).resolves.not.toThrow();
   });
 
-  describe('Session.addTransceiver function', () => {
-    it('adds a track', async () => {
-      const session = await newSession();
-      await session.start();
-      const { peer_connection_id }: any = await session.createPeerConnection({
-        name: PEER_CONNECTION_NAME,
-      });
-      await expect(
-        session.addTransceiver({
-          peer_connection_id,
-          track_id: '1',
-          track_label: 'FirstLabel',
-        }),
-      ).resolves.not.toThrow();
+  it('adds a track', async () => {
+    const session = await newSession();
+    await session.start();
+    const { peer_connection_id }: any = await session.createPeerConnection({
+      name: PEER_CONNECTION_NAME,
     });
+    await expect(
+      session.addTransceiver({
+        peer_connection_id,
+        track_id: '1',
+        track_label: 'FirstLabel',
+      }),
+    ).resolves.not.toThrow();
   });
 });
